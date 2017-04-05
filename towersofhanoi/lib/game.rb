e728555b117c942653bbff253900a041512258f7
App Academy Student <student@appacademy.io>
2017-04-05 14:44:48 -0700

    FINISHED

--- towersofhanoi/lib/game.rb
+++ towersofhanoi/lib/game.rb
@@ -1,10 +1,57 @@
 #note to self, left side is top (shift/unshift only)
 
 class Game
+  attr_accessor :towers
   def initialize(num)
+    @max = num
     @towers = [(1..num).to_a, [], []]
-
   end
 
+  def move_rings(start_tower, end_tower)
+    @towers[end_tower].unshift(towers[start_tower].shift)
+  end
+
+  def valid_move(start_tower, end_tower)
+    case true
+    when !start_tower.between?(0,2) || !end_tower.between?(0,2)
+      false
+    when @towers[start_tower].empty?
+      false
+    when @towers[start_tower].first > (@towers[end_tower].first || @max + 1 )
+      false
+    else
+      true
+    end
+  end
+
+  def won?
+    towers[0].empty? && (towers[1].empty? || towers[2].empty?)
+  end
+
+  def play
+    until won?
+      begin
+        render
+        puts "Select a tower to move from and a tower to move to."
+        from, to = gets.chomp.split(", ").map(&:to_i)
+        raise 'Invalid move' unless valid_move(from, to)
+      rescue
+        puts 'Please try again'
+        retry
+      end
+      move_rings(from, to)
+    end
+    render
+    puts "You've Won!"
+  end
+
+  def render
+    @towers.each_with_index do |tower,idx|
+      print "Tower: #{idx}"
+      p tower
+    end
+  end
 
 end
+game = Game.new(3)
+game.play
--- towersofhanoi/spec/game_spec.rb
+++ towersofhanoi/spec/game_spec.rb
@@ -18,4 +18,67 @@ describe Game do
       expect(game.towers).to eq([[2,3], [1], []])
     end
   end
+
+  describe '#valid_move' do
+    it 'returns a boolean' do
+      result = game.valid_move(0, 1)
+      expect([true,false].include?(result)).to eq(true)
+    end
+    it 'returns false when from tower is empty'do
+      expect(game.valid_move(1,2)).to eq(false)
+    end
+    it 'returns false when the recieving tower top ring is smaller than moved ring' do
+      game.move_rings(0,1)
+      expect(game.valid_move(0,1)).to eq(false)
+    end
+    it 'returns false when either tower does not exist' do
+      expect(game.valid_move(-1,5)).to eq(false)
+    end
+
+    it 'returns true for a valid move' do
+      expect(game.valid_move(0, 1)).to eq(true)
+    end
+  end
+
+  describe '#won?' do
+    let(:move){
+      game.move_rings(0,1)
+      game.move_rings(0,2)
+    }
+    it 'returns false if there are still rings on the first tower' do
+      expect(game.won?).to eq(false)
+    end
+    it 'returns false if neither of the last two towers are empty' do
+      game.move_rings(1, 2)
+      game.move_rings(0, 1)
+      expect(game.won?).to eq(false)
+    end
+    it ' it returns true if all of the rings are on tower two or three' do
+      game.towers = [[],[],[1,2,3]]
+      expect(game.won?).to eq(true)
+    end
+  end
+
+  # describe '#play' do
+  #   before(:each) do
+  #     game.towers = [[1],[],[2,3]]
+  #     puts "This happened before"
+  #   end
+  #
+  #   it 'calls to valid move' do
+  #     #game.towers = [[1],[],[2,3]]
+  #     expect(game).to receive(:valid_move)
+  #     game.play
+  #   end
+  #
+  #   it 'checks if the game is won' do
+  #     expect(game).to receive(:won?)
+  #     game.play
+  #   end
+
+
+
+  end
+
+
 end
